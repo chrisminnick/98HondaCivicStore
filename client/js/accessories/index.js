8f6302c9a1b491e1c9837c13b2c7388c32e05acd
Giovanni Tshibangu <alice@smith.com>
2024-01-22 11:45:24 -0500

    added a table to the accessory paghes

--- client/js/accessories/index.js
+++ client/js/accessories/index.js
@@ -1,17 +1,18 @@
-import { fetchAccessories } from '../utils/api.js';
+import { fetchAccessories } from "../utils/api.js";
 
 async function accessoriesPage() {
   const accessories = await fetchAccessories();
 
-  document.getElementById('page-title').innerHTML = 'Accessories';
+  document.getElementById("page-title").innerHTML = "Accessories";
 
   const accessoryList = accessories.map((accessory) => {
-    return '<li>' + accessory.accessoryName + '</li>';
+    return `<tr><td>${accessory.accessoryName}</td><td>${accessory.description}</td><td>${accessory.price}</td></tr>`;
   });
-  const accessoryHTMLList = accessoryList.join('');
 
-  document.getElementById('app').innerHTML =
-    '<ul id="product-list">' + accessoryHTMLList + '</ul>';
+  const tableHeader =
+    "<tr><th>Accessory Name </th><th>Accessory Desc </th><th>Accessory price </th></tr>";
+  document.getElementById("app").innerHTML =
+    "<table>" + tableHeader + accessoryList + "</table>";
 }
 
 export default accessoriesPage;
