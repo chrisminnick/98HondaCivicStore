77af6731d4792dcd900d40ff00c6259363e4cf34
Alice Smith <alice@smith.com>
2024-01-22 15:15:19 -0500

    adding to cart page

--- client/js/cart/index.js
+++ client/js/cart/index.js
@@ -1,4 +1,7 @@
 function cartPage() {
-  document.getElementById('page-title').innerHTML = 'Cart';
+  document.getElementById("page-title").innerHTML = "Cart";
+
+  document.getElementById("app").innerHTML =
+    "<h2>There is nothing in your cart. </h2>";
 }
 export default cartPage;
