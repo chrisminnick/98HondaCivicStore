6473eef5cd288d88c759c270d07cae002153a2ae
Giovanni Tshibangu <alice@smith.com>
2024-01-22 15:36:27 -0500

    added a table in the product page and updated the accessory page to have an error handling

--- client/js/products/index.js
+++ client/js/products/index.js
@@ -3,19 +3,20 @@ import { fetchProducts } from "../utils/api.js";
 async function productPage() {
   try {
     const products = await fetchProducts();
-  
-    document.getElementById('page-title').innerHTML = 'Products';
-  
+
+    document.getElementById("page-title").innerHTML = "Products";
+
     const productList = products.map((product) => {
-      return '<li>' + product.productName + '</li>';
+      return `<tr><td>${product.productName}</td><td>${product.description}</td><td>${product.price}</td></tr>`;
     });
-    const productHTMLList = productList.join('');
-  
-    document.getElementById('app').innerHTML =
-      '<ul id="product-list">' + productHTMLList + '</ul>';
-  }
-  catch(error) {
-    console.error("error with fetch", error)
+    // const productHTMLList = productList.join("");
+
+    const tableHeader =
+      "<tr><th>Product Name </th><th>Product Desc </th><th>Product price </th></tr>";
+    document.getElementById("app").innerHTML =
+      "<table>" + tableHeader + productList + "</table>";
+  } catch (error) {
+    console.error("error with fetch", error);
   }
 }
 export default productPage;
